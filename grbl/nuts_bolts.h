0e1e3cc0a4c8fdccee43646ea3019760aa5ec54f
Torsten Martinsen <torsten@bullestock.net>
2019-02-15 20:57:11 +0100

    Add missing parentheses in macros.

--- grbl/nuts_bolts.h
+++ grbl/nuts_bolts.h
@@ -62,11 +62,11 @@
 #define isequal_position_vector(a,b) !(memcmp(a, b, sizeof(float)*N_AXIS))
 
 // Bit field and masking macros
-#define bit(n) (1 << n)
+#define bit(n) (1 << (n))
 #define bit_true(x,mask) (x) |= (mask)
 #define bit_false(x,mask) (x) &= ~(mask)
-#define bit_istrue(x,mask) ((x & mask) != 0)
-#define bit_isfalse(x,mask) ((x & mask) == 0)
+#define bit_istrue(x,mask) (((x) & mask) != 0)
+#define bit_isfalse(x,mask) (((x) & mask) == 0)
 
 // Read a floating point value from a string. Line points to the input buffer, char_counter
 // is the indexer pointing to the current character of the line, while float_ptr is
